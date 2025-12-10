b5106622aa04cfe9c53e6d1119dd858949ae3a7b
amritha-279 <amritha.r2024cse@sece.ac.in>
2025-12-10 12:33:55 +0530

    Project on 10/12/2025

--- myapp2/src/App.js
+++ myapp2/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import "./App.css"; // <-- correct import since App.css is in src
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="header">
+        <div className="logo">
+          <img src="https://via.placeholder.com/40x20" alt="Logo" />
+        </div>
+        <div className="links">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/search">Search</Link>
+          <Link to="/cart">Cart</Link>
+        </div>
+      </nav>
+      <Outlet />
     </div>
   );
 }
--- myapp2/src/index.js
+++ myapp2/src/index.js
@@ -1,13 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Search from "./Pages/Search";
+import Cart from "./Pages/Cart";
+// just to create url and on which url which page should displayed
+const routerVariables = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/search",
+        element: <Search></Search>,
+      },
+      {
+        path: "/cart",
+        element: <Cart></Cart>,
+      },
+      {
+        path: "*",
+        element: <h1>Page not found Please check your URL</h1>,
+      },
+    ],
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routerVariables}></RouterProvider>
   </React.StrictMode>
 );
 
